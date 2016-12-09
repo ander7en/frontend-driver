520b3b5e0f1bec92af1ef58249d053b80f206cd9
Andrii Khrystian <andrii.khrystian@gmail.com>
2016-12-09 23:02:12 +0200

    local storage for integration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,43 @@
 angular.module('app.controllers', [])
   
-.controller('loginCtrl', ['$scope', '$stateParams', 'BackendService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('loginCtrl', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, localStorageService) {
     var self = this;
-    $scope.login = undefined;
+    $scope.credentials = { email: undefined, password: undefined };
+
+    init();
+    function init() {
+        if (!localStorageService.isSupported) {
+            $scope.login = "UNSUPPORTED";
+        } else if (localStorageService.get('email') != undefined) {
+            $scope.credentials.email = localStorageService.get('email');
+        }
+    }
+
+    $scope.submit = function () {
+        if (!localStorageService.isSupported) {
+            $scope.login = "UNSUPPORTED";
+        } else {
+            localStorageService.set('email', $scope.credentials.email)
+        }
+    };
+})
+   
+.controller('signupCtrl', ['$scope', '$stateParams', 'BackendService',  // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, localStorageService) {
+    var self = this;
+    $scope.fullName = undefined;
+    $scope.carInfo = undefined;
+    $scope.email = undefined;
     $scope.password = undefined;
 
     $scope.submit = function () {
         
     };
-}])
-   
-.controller('signupCtrl', ['$scope', '$stateParams', 'BackendService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
 
 }])
    
